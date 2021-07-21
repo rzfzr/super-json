d28a754ac5c4b6dc06fd1875d06e4f2d7ab80bee
Rafael Goncalves <capitao.rafa@gmail.com>
2021-07-21 00:12:38 -0300

    Editor outline

---
+++ src/components/Editor.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <div v-for="field in fields" :key="field.name">
+      <v-textarea :label="field.name" counter no-resize :rows="field.rows" v-model="field.value"></v-textarea>
+    </div>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Editor",
+  data: function () {
+    return {
+      fields: [
+        { name: "title", rows: 3, value: "" },
+        { name: "tags", rows: 5, value: "" },
+        { name: "description", rows: 10, value: "" },
+      ],
+    };
+  },
+});
+</script>
